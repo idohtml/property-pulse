92704f8e61d6a3b0aadcdd07c46044d8a43be01f
Jacob <Jacques.kanu@gmail.com>
2025-05-25 03:18:02 +0200

    properties pages

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,5 @@
+const Property = ({ params }: { params: { id: string } }) => {
+  return <div>Property Number:{params.id}</div>;
+};
+
+export default Property;
---
+++ app/properties/page.tsx
@@ -0,0 +1,3 @@
+export default function Proerties() {
+  return <div>Proerties</div>;
+}
