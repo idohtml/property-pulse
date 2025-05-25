9524dcf208b41c7f379cb96065b37c57ffb88df7
Jacob <Jacques.kanu@gmail.com>
2025-05-25 03:17:35 +0200

    auth client

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { client } from "@/db"; // your mongodb client
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI as string);
+const db = client.db();
 
 export const auth = betterAuth({
-  database: mongodbAdapter(client),
+  database: mongodbAdapter(db),
   socialProviders: {
     google: {
       prompt: "select_account",
